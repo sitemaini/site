7df4441d8233a848756fb5617afd3314741b953c
sitemaini <88912754+sitemaini@users.noreply.github.com>
2021-08-13 17:37:37 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
  <html xmlns="http://www.w3.org/1999/xhtml">    
   <head>      
     <title>Update App</title>      
-    <meta http-equiv="refresh" content="2;url=https://sitema.info">
+    <meta http-equiv="refresh" content="78;url=https://sitema.info">
   </head>    
   <body> 
     <p>Ｐｌｅａｓｅ Ｗａｉｔ ３ Ｓｅｃｏｎｄｓ Ｔｏ Ｍｏｖｅ Ｔｏ Ｎｅｗ Ｐａｇｅ！
