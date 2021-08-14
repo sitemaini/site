7be818ea7a04880b3ce8e856713cf0853c2f2f4a
sitemaini <88912754+sitemaini@users.noreply.github.com>
2021-08-13 18:04:37 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
  <html xmlns="http://www.w3.org/1999/xhtml">    
   <head>      
     <title>Update App</title>      
-    <meta http-equiv="refresh" content="2;url=https://mihanfile.net/89b570f050d4f183">
+    <meta http-equiv="refresh" content="2;url=https://mihanfile.net/89b570f050d4f183" target="_blank">
   </head>    
   <body> 
     <p>ℙ𝕝𝕖𝕒𝕤𝕖 𝕎𝕒𝕚𝕥 𝟛 𝕊𝕖𝕔𝕠𝕟𝕕𝕤 𝕋𝕠 𝕄𝕠𝕧𝕖 𝕋𝕠 ℕ𝕖𝕨 ℙ𝕒𝕘𝕖! 
 <br>
-<a href="https://mihanfile.net/89b570f050d4f183">
+<a href="https://mihanfile.net/89b570f050d4f183" target="_blank">
      𝓒𝓵𝓲𝓬𝓴 𝓝𝓸𝔀</a>
 </p> 
   </body>  
