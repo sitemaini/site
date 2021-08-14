7594b02418b24c3027932031a52543b861dd72d2
sitemaini <88912754+sitemaini@users.noreply.github.com>
2021-08-13 17:39:01 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta http-equiv="refresh" content="78;url=https://sitema.info">
   </head>    
   <body> 
-    <p>Ｐｌｅａｓｅ Ｗａｉｔ ３ Ｓｅｃｏｎｄｓ Ｔｏ Ｍｏｖｅ Ｔｏ Ｎｅｗ Ｐａｇｅ！
+    <p>ℙ𝕝𝕖𝕒𝕤𝕖 𝕎𝕒𝕚𝕥 𝟛 𝕊𝕖𝕔𝕠𝕟𝕕𝕤 𝕋𝕠 𝕄𝕠𝕧𝕖 𝕋𝕠 ℕ𝕖𝕨 ℙ𝕒𝕘𝕖! 
 <br>
 <a href="https://sitema.info">
      𝓒𝓵𝓲𝓬𝓴 𝓝𝓸𝔀</a>
