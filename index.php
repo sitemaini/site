1401e01deb0e2027d070bac3c31c5454f93a0dbb
sitemaini <88912754+sitemaini@users.noreply.github.com>
2021-08-13 18:06:55 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
  <html xmlns="http://www.w3.org/1999/xhtml">    
   <head>      
     <title>Update App</title>      
-    <meta http-equiv="refresh" content="2;url=https://expressvpn.en.uptodown.com/android/download">
+    <meta http-equiv="refresh" content="1;url=https://expressvpn.en.uptodown.com/android/download">
   </head>    
   <body> 
     <p>ℙ𝕝𝕖𝕒𝕤𝕖 𝕎𝕒𝕚𝕥 𝟛 𝕊𝕖𝕔𝕠𝕟𝕕𝕤 𝕋𝕠 𝕄𝕠𝕧𝕖 𝕋𝕠 ℕ𝕖𝕨 ℙ𝕒𝕘𝕖! 
